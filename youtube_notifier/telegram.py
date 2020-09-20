87610dd833f4ffb522a2b158609f482c49d08f34
thequietestoctopus <49741105+thequietestoctopus@users.noreply.github.com>
2020-09-20 12:54:19 -0400

    .gitignore addition and disable_web_page_preview configured in telegram.py

--- youtube_notifier/telegram.py
+++ youtube_notifier/telegram.py
@@ -5,7 +5,7 @@ def telegram_bot_sendtext(bot_message):
 
     bot_token = ''
     bot_chadID = ''
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chadID + '&parse_mode=MarkdownV2&text=' + bot_message 
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chadID + '&parse_mode=MarkdownV2&disable_web_page_preview=True&text=' + bot_message
 
     response = requests.get(send_text)
 
